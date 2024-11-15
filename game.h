29e05958e65e255c355bc26f84305659473879f4
Shane Wilkerson <swilkerson@odin.cs.csubak.edu>
2024-11-14 21:49:41 -0800

    We are so back boys

--- game.h
+++ game.h
@@ -1,21 +1,108 @@
 #ifndef GAME_H
 #define GAME_H
 
-//Forward declarations of classes
-class Global;
-class Ball;
-class Bullet;
-class Game;
-class Level;
-class X11_wrapper;
+#include <iostream>
+#include <cstring>
+#include <ctime>
+#include <cmath>
+#include "fonts.h"
+#include "log.h"
+#include "sgonzales.h"
+#include <time.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <cstdlib>
 
-//Declare the external variables that are defined in Peaman.cpp
-extern Global g;       
-extern Ball bal;        
-extern Game ga;         
-extern Level lev;       
-extern X11_wrapper x11; 
+typedef double Vec[3];
+const double physicsRate = 1.0 / 60.0;
+const double oobillion = 1.0 / 1e9;
+extern struct timespec timeStart, timeCurrent;
+extern struct timespec timePause;
+extern double physicsCountdown;
+extern double timeSpan;
+extern double timeDiff(struct timespec *start, struct timespec *end);
+extern void timeCopy(struct timespec *dest, struct timespec *source);
 
-//extern Gun* currentGun;
+const double BULLET_LIFESPAN = 3.0;
+const int MAX_BULLETS = 99;
+
+class Global {
+public:
+    int useless_Var;
+    unsigned char keys[65536];
+    int xres, yres;
+    double delay;
+    GLuint MenuTexture;
+    GLuint silhouetteTexture;
+    GLuint PlayTexture;
+    int playPress;
+    int silhouette;
+    int menu;
+    int mx, my;
+
+    Global();
+
+};
+
+class Ball {
+public:
+    Vec vel;
+    Vec dir;
+    float angle;
+    float color[3];
+    float pos[2];
+    float movement[2];
+    double camera[2];
+
+    Ball();
+
+};
+
+class Bullet {
+public:
+   Vec pos;
+   Vec vel;
+    float color[3];
+    struct timespec time;
+public:
+    Bullet();
+};
+
+class Game {
+public:
+    Ball bal;
+    Bullet *barr;
+    int nbullets;
+    struct timespec bulletTimer;
+
+public:
+    Game();
+
+    ~Game();
+
+    // Function to check if a bullet has expired
+    void check_bullet_lifetime();
+
+
+
+
+
+
+
+};
+
+extern Global g;
+extern Ball bal;
+extern Game ga;
+
+void fire_bullet(int mx, int my);
+void update_bullets();
+
+
 
 #endif // GAME_H
--- swilkerson.cpp
+++ swilkerson.cpp
@@ -1,18 +1,76 @@
 //Shane Wilkerson
 
-#include "fonts.h"
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <cmath>
+#include <math.h>
+#include <time.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "fonts.h"
+#include "log.h"
+#include "sgonzales.h"
 #include "game.h"
 
 using namespace std;
 
-extern Global g;
-extern Ball bal;
-extern Game ga;
-extern Bullet myBullet;
+void fire_bullet(int mx, int my) {
+    if (ga.nbullets < MAX_BULLETS) {
+        Bullet new_bullet;
+
+        //Set the bullet's initial position to the player's position
+        new_bullet.pos[0] = bal.pos[0];
+        new_bullet.pos[1] = bal.pos[1];
+
+        //Calculate the direction from player to mouse (g.mx, g.my)
+        float dx = mx - new_bullet.pos[0];
+        float dy = new_bullet.pos[1] - my;
+        float magnitude = sqrt(dx * dx + dy * dy);
+
+        //set the bullet's velocity
+        new_bullet.vel[0] = dx / magnitude * 9.5;  //adjust speed here
+        new_bullet.vel[1] = dy / magnitude * 9.5;
+
+        //current time for the bullet's creation
+        clock_gettime(CLOCK_REALTIME, &new_bullet.time);
+
+        //add the new bullet to the array
+        ga.barr[ga.nbullets++] = new_bullet;
+    }
+}
+
+void update_bullets() {
+    //move each bullet based on its velocity
+    for (int i = 0; i < ga.nbullets; ++i) {
+        Bullet &bullet = ga.barr[i];
+        bullet.pos[0] += bullet.vel[0];  //Update X position
+        bullet.pos[1] += bullet.vel[1];  //Update Y position
+    }
+
+    //remove expired bullets
+    ga.check_bullet_lifetime();
+}
+int currentWeapon = 1;
 
 int shane_show = 0;
 
+void show_gun(int x, int y)
+{
+    Rect r;
+    r.bot = y;
+    r.left = x;
+    r.center = 0;
+    ggprint8b(&r, 16, 0x00ff0000, "AR");
+}
+
+/*
 enum GunType {
     AR,       
     Shotgun,  
@@ -67,7 +125,7 @@ public:
 };
 
 Gun* currentGun = nullptr;
-
+*/
 void show_my_featureSW(int x, int y)
 {
     //draw a rectangle
